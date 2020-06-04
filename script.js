a8632a79394d05a445cdf28796a87925f00496d9
Juan Campos <juancampos.bsi@outlook.com>
2020-06-04 20:20:12 -0300

    cadastro

--- script.js
+++ script.js
@@ -18,3 +18,39 @@ for (let card of cards) {
 document.querySelector(".modal-close").addEventListener("click", function() {
   modalOverlay.classList.remove("active");
 });
+
+
+function SomenteNumero(e){
+  var tecla=(window.event)?event.keyCode:e.which;
+  if((tecla>47 && tecla<58)) return true;
+  else{
+  if (tecla==8 || tecla==0) return true;
+  else  return false;
+  }
+ }
+
+ function validacaoEmail(field) {
+  usuario = field.value.substring(0, field.value.indexOf("@"));
+  dominio = field.value.substring(field.value.indexOf("@")+ 1, field.value.length);
+   
+  if ((usuario.length >=1) &&
+      (dominio.length >=3) && 
+      (usuario.search("@")==-1) && 
+      (dominio.search("@")==-1) &&
+      (usuario.search(" ")==-1) && 
+      (dominio.search(" ")==-1) &&
+      (dominio.search(".")!=-1) &&      
+      (dominio.indexOf(".") >=1)&& 
+      (dominio.lastIndexOf(".") < dominio.length - 1)) {
+  document.getElementById("msgemail").innerHTML="E-mail válido";
+  alert("E-mail valido");
+  }
+  else{
+  document.getElementById("msgemail").innerHTML="<font color='red'>E-mail inválido </font>";
+  alert("E-mail invalido");
+  }
+  }
+
+  function limitarInput(obj) {
+    obj.value = obj.value.substring(0,2); 
+  }
